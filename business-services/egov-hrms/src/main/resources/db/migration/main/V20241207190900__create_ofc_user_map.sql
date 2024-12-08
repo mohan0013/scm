a8d2b3f3b2a55578f3a2a9b045fa5e1e2696023f
prasunpwc <prasun.naiya@pwc.com>
2024-12-08 14:26:38 +0530

    hrms and mdms changes

--- business-services/egov-hrms/src/main/resources/db/migration/main/V20241207190900__create_ofc_user_map.sql
+++ business-services/egov-hrms/src/main/resources/db/migration/main/V20241207190900__create_ofc_user_map.sql
@@ -1,6 +1,6 @@
 create table eg_user_office_map(
 id INTEGER PRIMARY KEY GENERATED ALWAYS AS identity,
-user_id integer references eg_hrms_employee(id),
+user_id integer,
 office_id integer references eg_offices(id),
 active boolean,
 NAA_code integer references eg_user(id),
